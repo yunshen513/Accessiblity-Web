292d48ea96496bd2dbf7968318ab6eece2667ccf
yunshen513 <yunshen513@gmail.com>
2023-01-15 23:49:18 +1100

    add layer via reading data

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Map from "./components/Map";
 import './Page.css'
 
-const Page = () => (
-    <div className="layout_map">
-        <div className="map_title">Page test</div>
-        <div className="map_content">
-            <Map />
+const Page = () => {
+    const [filters, setFilters] = useState(0)
+    const selectedValue = useRef(0);
+
+    return (
+      <div className="layout_map">
+        <div className="map_title">Map Type {filters}</div>
+        <div className="map_control">
+          <select onChange={(e) => (selectedValue.current = e.target.value)}>
+            <option value={0}>All</option>
+            <option value={1}>Accessibility > 1</option>
+            <option value={3}>Accessibility > 3</option>
+          </select>
+          <button onClick={() => setFilters(selectedValue.current)}>
+            Gimme the map
+          </button>
         </div>
-    </div>
-);
+        <div className="map_content">
+          <Map selectedFilters={filters} />
+        </div>
+      </div>
+    );
+};
 
 export default Page;
--- src/components/Page/components/Map/Map.js
+++ src/components/Page/components/Map/Map.js
@@ -1,146 +1,78 @@
-import React, { useRef, useEffect, useState } from "react";
-import 'mapbox-gl/dist/mapbox-gl.css';
-import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
-import './Map.css';
-import myData from './trees.geojson'
+import React, { useEffect, useRef } from "react";
+import "./Map.css";
+import "mapbox-gl/dist/mapbox-gl.css";
+import mapboxgl from "mapbox-gl";
+import dataSource from "./suburb-data.json";
 
-mapboxgl.accessToken = 'pk.eyJ1IjoieXVuc2hlbjUxMyIsImEiOiJjbGNrYWpudHAzMjdvM3FwNDc2Y3JoOGxiIn0.aRCWmrFlMnX-IfvMk3lrbw';
+mapboxgl.accessToken =
+    "pk.eyJ1IjoieXVuc2hlbjUxMyIsImEiOiJjbGNrYWpudHAzMjdvM3FwNDc2Y3JoOGxiIn0.aRCWmrFlMnX-IfvMk3lrbw";
 
-const Map = () => {
-        const mapContainer = useRef(null);
-        const map = useRef(null);
-        const [lng, setLng] = useState(-79.999732);
-        const [lat, setLat] = useState(40.4374);
-        const [zoom, setZoom] = useState(12);
+const Map = ({ selectedFilters }) => {
+    const map = useRef(null);
+    const mapContainer = useRef(null);
 
-        useEffect(() => {
-            if (map.current) return; // initialize map only once
+    const rawData = dataSource.features;
+    console.log(selectedFilters);
 
-            map.current = new mapboxgl.Map({
+    useEffect(() => {
+        //if (map.current) return;
+
+        //initiate the map
+        map.current = new mapboxgl.Map({
             container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/dark-v11',
-            center: [lng, lat],
-            zoom: zoom
-            });
-
-            map.current.on('load', () => {
-                map.current.addSource('trees', {
-                  type: 'geojson',
-                  data: myData
-                });
-
-                // add heatmap layer here
-                 map.current.addLayer(
-                    {
-                      id: 'trees-heat',
-                      type: 'heatmap',
-                      source: 'trees',
-                      maxzoom: 15,
-                      paint: {
-                        // increase weight as diameter breast height increases
-                        'heatmap-weight': {
-                          property: 'dbh',
-                          type: 'exponential',
-                          stops: [
-                            [1, 0],
-                            [62, 1]
-                          ]
-                        },
-                        // increase intensity as zoom level increases
-                        'heatmap-intensity': {
-                          stops: [
-                            [11, 1],
-                            [15, 3]
-                          ]
-                        },
-                        // assign color values be applied to points depending on their density
-                        'heatmap-color': [
-                          'interpolate',
-                          ['linear'],
-                          ['heatmap-density'],
-                          0,
-                          'rgba(236,222,239,0)',
-                          0.2,
-                          'rgb(208,209,230)',
-                          0.4,
-                          'rgb(166,189,219)',
-                          0.6,
-                          'rgb(103,169,207)',
-                          0.8,
-                          'rgb(28,144,153)'
-                        ],
-                        // increase radius as zoom increases
-                        'heatmap-radius': {
-                          stops: [
-                            [11, 15],
-                            [15, 20]
-                          ]
-                        },
-                        // decrease opacity to transition into the circle layer
-                        'heatmap-opacity': {
-                          default: 1,
-                          stops: [
-                            [14, 1],
-                            [15, 0]
-                          ]
-                        }
-                      }
-                    },
-                    'waterway-label'
-                  );
-                // add circle layer here
-                map.current.addLayer(
-                    {
-                      id: 'trees-point',
-                      type: 'circle',
-                      source: 'trees',
-                      minzoom: 14,
-                      paint: {
-                        // increase the radius of the circle as the zoom level and dbh value increases
-                        'circle-radius': {
-                          property: 'dbh',
-                          type: 'exponential',
-                          stops: [
-                            [{ zoom: 15, value: 1 }, 5],
-                            [{ zoom: 15, value: 62 }, 10],
-                            [{ zoom: 22, value: 1 }, 20],
-                            [{ zoom: 22, value: 62 }, 50]
-                          ]
-                        },
-                        'circle-color': {
-                          property: 'dbh',
-                          type: 'exponential',
-                          stops: [
-                            [0, 'rgba(236,222,239,0)'],
-                            [10, 'rgb(236,222,239)'],
-                            [20, 'rgb(208,209,230)'],
-                            [30, 'rgb(166,189,219)'],
-                            [40, 'rgb(103,169,207)'],
-                            [50, 'rgb(28,144,153)'],
-                            [60, 'rgb(1,108,89)']
-                          ]
-                        },
-                        'circle-stroke-color': 'white',
-                        'circle-stroke-width': 1,
-                        'circle-opacity': {
-                          stops: [
-                            [14, 0],
-                            [15, 1]
-                          ]
-                        }
-                      }
-                    },
-                    'waterway-label'
-                  );
-
-              });
+            center: [144.9631, -37.8136],
+            zoom: 10,
+            style: "mapbox://styles/mapbox/streets-v11",
         });
 
-        return (
-            <div>
-            <div ref={mapContainer} className="map-container" />
-            </div>
-        );
-}
+        map.current.on("load", () => {
+            //filter data
+            const data = {
+                type: "FeatureCollection",
+                features: rawData.filter((item) => {
+                    return item.properties.AREASQKM21 >= selectedFilters;
+                }),
+            };
+            console.log(data);
+
+            //define data source
+            map.current.addSource("suburb-data", {
+                type: "geojson",
+                data: data,
+            });
+            //style map using dataset
+            map.current.addLayer({
+                id: "suburbs",
+                source: "suburb-data",
+                type: "fill",
+                paint: {
+                    "fill-color": [
+                        "step",
+                        ["get", "AREASQKM21"],
+                        "#feda76",
+                        0.5,
+                        "#feb24d",
+                        1,
+                        "#fd8b3a",
+                        2,
+                        "#fc4f2c",
+                        3,
+                        "#e41b1e",
+                        4,
+                        "#bd0026",
+                        6,
+                        "#88021d",
+                        8,
+                        "#5a0214",
+                    ],
+                    "fill-opacity": 0.8,
+                    "fill-outline-color": "#FFFFFF",
+                },
+            });
+        });
+    });
+
+    return <div ref={mapContainer} className="map-container"></div>;
+};
 
 export default Map;
